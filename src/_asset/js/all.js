1cdef8d7942b3378bcc58fe23ca95dc7ab62fde7
robertoalx <romaniarobertverde@yahoo.com>
2018-10-29 10:48:47 +0000

    javaScript is not working. Start to fix it.

--- src/_asset/js/all.js
+++ src/_asset/js/all.js
@@ -1,13 +1,15 @@
+
 let iframeList = Array.from(document.querySelectorAll('iframe'));
 let handlerFunction = () => {
-    iframeList.map (iframe => {
-        iframe.src = iframe.dataset.src;
-    });
+  //- set the src attribute value to be the value of data-src
+  iframeList.map(iframe => {
+    iframe.src = iframe.dataset.src;
+  });
+  document.removeEventListener('scroll', handlerFunction);
 };
-
 let scrollListener = document.addEventListener('scroll', handlerFunction);
 
 let bookSpeakerList = Array.from(document.querySelectorAll('.book-me'));
-bookSpeakerList.map(bookSpeaker => bookSpeakerList.addEventListener('click', e => {
-    alert(2)
+bookSpeakerList.map(bookSpeaker => bookSpeaker.addEventListener('click', e => {
+  bookSpeaker.href = 'mai' + 'lto:booking' + '@robertoblake.com' + '?Subject=' + 'Bookng%20Speaking' + '%20Session';
 }))
