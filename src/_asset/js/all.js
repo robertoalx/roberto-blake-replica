1fc7750a707ca5b49bc656781bffb5351c4fa5b9
robertoalx <romaniarobertverde@yahoo.com>
2018-10-30 09:39:31 +0000

    All I had to do is to leave some empty lines between variables and functions and now is working.

--- src/_asset/js/all.js
+++ src/_asset/js/all.js
@@ -1,5 +1,5 @@
-
 let iframeList = Array.from(document.querySelectorAll('iframe'));
+
 let handlerFunction = () => {
   //- set the src attribute value to be the value of data-src
   iframeList.map(iframe => {
@@ -10,6 +10,7 @@ let handlerFunction = () => {
 let scrollListener = document.addEventListener('scroll', handlerFunction);
 
 let bookSpeakerList = Array.from(document.querySelectorAll('.book-me'));
+
 bookSpeakerList.map(bookSpeaker => bookSpeaker.addEventListener('click', e => {
   bookSpeaker.href = 'mai' + 'lto:booking' + '@robertoblake.com' + '?Subject=' + 'Bookng%20Speaking' + '%20Session';
 }))
